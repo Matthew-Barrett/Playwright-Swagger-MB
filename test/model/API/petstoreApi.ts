143537ed3102e21cb89cdcf2b7846b4e9fac3c1a
Matt <mbarrett@planittesting.com>
2025-02-09 16:00:55 +1300

    put all API methods inside try catch block

--- test/model/API/petstoreApi.ts
+++ test/model/API/petstoreApi.ts
@@ -8,18 +8,38 @@ export class PetstoreAPI extends BaseAPI {
 
   
   async addPet(petData: object): Promise<APIResponse> {
+    try{
     return this.post(`${UrlProperties.API_BASE_URL}/pet`, petData);
+    } catch(error) {
+      console.log('Error adding pet:', error);
+      throw error;
   }
+}
 
   async getPetById(petId: number): Promise<APIResponse> {
+    try {
     return this.get(`${UrlProperties.API_BASE_URL}/pet/${petId}`);
+    } catch(error) {
+      console.log('Error getting pet by ID:', error);
+      throw error;
   }
+}
 
   async updatePet(petData: object): Promise<APIResponse> {
+    try {
     return this.put(`${UrlProperties.API_BASE_URL}/pet`, petData);
+    } catch(error) {
+      console.log('Error updating pet:', error);
+      throw error;
   }
+}
 
   async deletePet(petId: number): Promise<APIResponse> {
+    try {
     return this.delete(`${UrlProperties.API_BASE_URL}/pet/${petId}`);
+    } catch(error) {
+      console.log('Error deleting pet:', error);
+      throw error;
   }
+}
 }
